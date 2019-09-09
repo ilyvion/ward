8db8a8d4efc5443429135a5e71c4870493e1df6f
Alexander Krivács Schrøder <alexschrod@gmail.com>
2019-09-09 18:44:20 +0200

    Add code documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,57 @@
+//! This crate exports two macros, which are intended to replicate the functionality of Swift's
+//! guard expression with `Option<T>`. They both do similar things, but the `ward!` macro
+//! technically has more use cases than the `guard!` macro, because it returns a value instead of
+//! creating a variable.
+
+/// Returns the contents of a `Option<T>`'s `Some(T)`, otherwise it returns early from the function.
+///
+/// # Examples
+/// ```
+/// # #[macro_use] extern crate ward;
+/// #
+/// # fn main() {
+/// // res is set to sut's contents
+/// let sut = Some("test");
+/// let res = ward!(sut);
+/// assert_eq!("test", res);
+/// # }
+/// ```
+/// ```
+/// # #[macro_use] extern crate ward;
+/// #
+/// # fn main() {
+/// // Because sut is None, the ward! returns early
+/// let sut = None;
+/// let res = ward!(sut);
+/// unreachable!();
+/// # }
+/// ```
+/// ```
+/// # #[macro_use] extern crate ward;
+/// #
+/// # fn main() {
+/// // Because sut is None, the ward!'s else branch will be run before it returns early
+/// let sut = None;
+/// let _res = ward!(sut, else {
+///     # assert!(true);
+///     println!("This code will run!");
+/// });
+/// unreachable!();
+/// # }
+/// ```
+/// ```
+/// # #[macro_use] extern crate ward;
+/// #
+/// # fn main() {
+/// // Unlike guard!, ward! can be used as an expression
+/// let sut = Some("test");
+/// print(ward!(sut));
+/// # }
+///
+/// fn print(text: &str) {
+///     println!("{}", text);
+/// }
+/// ```
 #[macro_export]
 macro_rules! ward {
     ($o:expr) => {
@@ -8,6 +62,45 @@ macro_rules! ward {
     };
 }
 
+/// Creates a variable with the contents of a `Option<T>`'s `Some(T)`, otherwise it returns early
+/// from the function.
+///
+/// # Examples
+/// ```
+/// # #[macro_use] extern crate ward;
+/// #
+/// # fn main() {
+/// // res is set to sut's contents
+/// let sut = Some("test");
+/// guard!(let res = sut);
+/// assert_eq!("test", res);
+/// # }
+/// ```
+///
+/// ```
+/// # #[macro_use] extern crate ward;
+/// #
+/// # fn main() {
+/// // Because sut is None, the guard! returns early
+/// let sut = None;
+/// guard!(let mut res = sut);
+/// unreachable!();
+/// # }
+/// ```
+///
+/// ```
+/// # #[macro_use] extern crate ward;
+/// #
+/// # fn main() {
+/// // Because sut is None, the guard!'s else branch will be run before it returns early
+/// let sut = None;
+/// guard!(let _res = sut, else {
+///     # assert!(true);
+///     println!("This code will run!");
+/// });
+/// unreachable!();
+/// # }
+/// ```
 #[macro_export]
 macro_rules! guard {
     (let $result:ident = $o:expr) => {
